a26f5df68d97f0499b3fb8f89295b39403421b26
Kovács Sámuel <sunikovacs@gmail.com>
2022-02-02 09:10:47 +0100

    Every file operation works

--- src/main.cpp
+++ src/main.cpp
@@ -397,21 +397,65 @@ void loop()
 
   int rootFileCount = 0;
   if (!root.open("/")) {
-    Serial.println("open root");
+    Serial.println("Open root failed");
   }
+
+
+  //először el kell távolítani a már létező fájlokat!!!, emiatt nem működött először
+  if (sd.exists("Folder1")) {
+    if (sd.exists("Folder1/file1.txt")) {
+      Serial.println("Folder1/file1.txt is already existing");
+
+      // Change volume working directory to Folder1.
+      if (!sd.chdir("Folder1")) {
+        Serial.println("chdir failed for Folder1.");
+      }
+      Serial.println("chdir to Folder1");
+
+
+      // Remove files from current directory.
+      if (!sd.remove("file1.txt")) {
+        Serial.println("remove failed");
+      }
+      Serial.println("file1.txt removed.");
+
+
+      // Change current directory to root.
+      if (!sd.chdir()) {
+        Serial.println("chdir to root failed.");
+      }
+
+      // Remove Folder1.
+      if (!sd.rmdir("Folder1")) {
+        Serial.println("rmdir for Folder1 failed");
+      }
+      Serial.println("Folder1 removed.");
+      
+    }
+  }
+
+/***********************************************/
+
   // Create a new folder.
   if (!sd.mkdir("Folder1")) {
     Serial.println("Create Folder1 failed");
   }
 
-  // Create a file in Folder1 using a path.
-  if (!file.open("Folder1/file1.txt", O_WRONLY | O_CREAT)) {
-    Serial.println("create Folder1/file1.txt failed");
+  if (!sd.mkdir("Folder2")) {
+    Serial.println("Create Folder2 failed");
   }
 
+  // Create a file in Folder1 using a path.
+  if (!file.open("Folder1/file1.txt", O_WRONLY | O_CREAT)) {
+    Serial.println("Create Folder1/file1.txt failed");
+  }
+  Serial.println("Created Folder1/file1.txt");
   file.println("Hello 1, 2, 3");
-  file.close();
-  cout << F("Created Folder1/file1.txt\n");
+  Serial.println("Hello 1, 2, 3 written to Folder1/file1.txt");
 
+  Serial.print("Size of Folder1/file1.txt: "); Serial.println(file.fileSize());
+
+  file.close();
+  Serial.println("Folder1/file1.txt closed");
 
 }
