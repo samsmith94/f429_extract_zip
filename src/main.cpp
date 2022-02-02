d8d3d0c1785beb99c2d65e130f35af5615bf67ca
Kovács Sámuel <sunikovacs@gmail.com>
2022-02-02 18:35:23 +0100

    add hexdump library

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 
+#include <HexDump.h>
 //#define COMPILE_ONLY_SD
 #ifdef COMPILE_ONLY_SD
 /******************************************************************************/
@@ -131,8 +132,6 @@ void setup()
 void loop() {}
 
 /******************************************************************************/
-/******************************************************************************/
-/******************************************************************************/
 
 #else
 
@@ -158,6 +157,8 @@ static File myfile;
 
 static File myfileAnother;
 
+
+static File binFile;
 //
 // Callback functions needed by the unzipLIB to access a file system
 // The library has built-in code for memory-to-memory transfers, but needs
@@ -170,7 +171,7 @@ void * myOpen(const char *filename, int32_t *size) {
   root.open("/");
   myfileAnother.open(filename);
   *size = (uint32_t)myfileAnother.fileSize();
-  Serial.print("File size in myOpen: "); Serial.println(*size);
+  //Serial.print("File size in myOpen: "); Serial.println(*size);
 
   return (void *)&myfileAnother;
 }
@@ -200,16 +201,15 @@ int32_t mySeek(void *p, int32_t position, int iType) {
   }
 }
 
+
+#define BUFF_SIZE 512
+static uint8_t l_Buff[BUFF_SIZE];
+
 void setup() {
   Serial.begin(115200);
 
   while (!Serial && millis() < 3000);
   Serial.println("Search for ZIP files on the SD card");
-
-  // while (!SD.begin(4)) {
-  //   Serial.println("Unable to access SD Card");
-  //   delay(1000);
-  // }
   
   if (!sd.begin(SD_CONFIG))
   {
@@ -221,8 +221,6 @@ void setup() {
   {
     Serial.println("Open root failed");
   }
-
-
 
   //először el kell távolítani a már létező fájlokat!!!, emiatt nem működött először
   if (sd.exists("Folder1"))
@@ -290,8 +288,7 @@ void setup() {
   Serial.println("****************************************");
 
 
-  /**********************/
-  //EZ SAJNOS MÉG NEM MŰKÖDIK...
+  //////////////////////////////////////////////////////////////////////////////
   int rc;
   char szComment[256], szName[256];
   unz_file_info fi;
@@ -301,73 +298,74 @@ void setup() {
   {
     Serial.println("Opening application.zip failed");
   }
-  Serial.println("Openend application.zip");
+  Serial.println("Openened application.zip");
   rc = rc = zip.openZIP(name, myOpen, myClose, myRead, mySeek);
   if (rc == UNZ_OK) {
-    Serial.print("ZIP file found");
+    Serial.println("ZIP file found!");
 
     // Display the global comment and all of the filenames within
     rc = zip.getGlobalComment(szComment, sizeof(szComment));
-    Serial.print("Global comment: "); Serial.println(szComment); Serial.println("Files in this archive:");
+    Serial.print("Files in this archive: ");
     zip.gotoFirstFile();
     rc = UNZ_OK;
     rc = zip.getFileInfo(&fi, szName, sizeof(szName), NULL, 0, szComment, sizeof(szComment));
 
     if (rc == UNZ_OK) {
-      Serial.print(szName); Serial.print(" - "); Serial.print(fi.compressed_size, DEC); Serial.print("/"); Serial.println(fi.uncompressed_size, DEC);
+      Serial.println(szName);
+      Serial.print("Compressed size: "); Serial.println(fi.compressed_size, DEC);
+      Serial.print("Uncompressed size: "); Serial.println(fi.uncompressed_size, DEC);
+      
     }
+
+    //ez fog kelleni majd nekem a chunkwise dologhoz!!!
+    //int UNZIP::readCurrentFile(uint8_t *buffer, int iLength)
+
+    //a mySeek-et is tudom hívni :D
+    // mySeek(void *p, int32_t position, int iType)
+    //         myFile,         offset,   SEEK_SET
+
+
+    // Create a file in Folder1 using a path.
+    if (!binFile.open("application.bin", O_WRONLY | O_CREAT))
+    {
+      Serial.println("Create application.bin failed");
+    }
+    else
+    {
+      Serial.println("application.bin created");
+    }
+    
+
+    zip.openCurrentFile();
+
+    // //1. blokk
+    // //mySeek(void *p, int32_t position, int iType)
+    // EZ ELRONT VALAMIT...: mySeek(&myfile, 0, SEEK_SET);
+    //mySeek(&myfile, 0, SEEK_SET);
+    //myfile.seekSet(0);
+    int readSize = zip.readCurrentFile(l_Buff, BUFF_SIZE);
+    Serial.print("Bytes read in buffer: "); Serial.println(readSize);
+    binFile.write(l_Buff, BUFF_SIZE);
+
+    HexDump(Serial, l_Buff, sizeof(l_Buff));
+
+    //Serial.print("First chunk (0): "); Serial.print((char*)l_Buff[0]);
+
+    // //2. blokk
+    // mySeek(&myfile, 1024, SEEK_SET);
+    // myfile.seekSet(1024);
+    // readSize = zip.readCurrentFile(l_Buff, BUFF_SIZE);
+    // Serial.print("Bytes read in buffer: "); Serial.println(readSize);
+    // binFile.write(l_Buff, BUFF_SIZE);
+    
+    binFile.close();
+    // Serial.print("Second chunk (1024): "); Serial.print((char*)l_Buff[0]);
     zip.closeZIP();
   }
 }
 
 // Main loop, scan for all .PNG files on the card and display them
-void loop() {
-  // int rc, filecount = 0;
-  // char szComment[256], szName[256];
-  // unz_file_info fi;
-  
-  // File dir = SD.open("/");
-  // while (true) {
-  //   File entry = dir.openNextFile();
-  //   if (!entry) break;
-
-  //   if (entry.isDirectory() == false) {
-  //     const char *name = entry.name();
-  //     const int len = strlen(name);
-
-  //     if (len > 3 && strcmp(name + len - 3, "ZIP") == 0) {
-  //       Serial.print("File: "); Serial.println(name);
-  //       rc = rc = zip.openZIP(name, myOpen, myClose, myRead, mySeek);
-
-  //       if (rc == UNZ_OK) {
-  //         Serial.print("found zip file: "); Serial.println(name);
-
-  //         // Display the global comment and all of the filenames within
-  //         rc = zip.getGlobalComment(szComment, sizeof(szComment));
-  //         Serial.print("Global comment: "); Serial.println(szComment); Serial.println("Files in this archive:");
-  //         zip.gotoFirstFile();
-  //         rc = UNZ_OK;
-
-  //         while (rc == UNZ_OK) { // Display all files contained in the archive
-  //           rc = zip.getFileInfo(&fi, szName, sizeof(szName), NULL, 0, szComment, sizeof(szComment));
-
-  //           if (rc == UNZ_OK) {
-  //             Serial.print(szName); Serial.print(" - "); Serial.print(fi.compressed_size, DEC); Serial.print("/"); Serial.println(fi.uncompressed_size, DEC);
-  //           }
-  //           rc = zip.gotoNextFile();
-  //         } // while more files...
-  //         zip.closeZIP();
-  //       }
-  //       filecount = filecount + 1;
-  //     }
-  //   }
-  //   entry.close();
-  // }
-  // if (filecount == 0) {
-  //   Serial.println("No .ZIP files found");
-  // }
-  // delay(2000);
-}
+void loop() {}
 
 /******************************************************************************/
 #endif
